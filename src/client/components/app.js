0209c7ab112e24760c5b9f450a87ba6045bd9a58
Alexey Sidorovitch <sidorovitch@gmail.com>
2018-04-20 20:38:26 +0300

    hot module reload react

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -3,6 +3,9 @@ import './app.scss';
 import React, { Component } from 'react';
 
 class AppContainer extends Component {
+	componentDidMount() {
+		console.log('HEY THERE 23232323');
+	}
 	render() {
 		return <h1>Hello World</h1>;
 	}
