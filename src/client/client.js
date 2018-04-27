edf873dc723609c2228397031deecf21705e9c72
Alexey Sidorovitch <sidorovitch@gmail.com>
2018-04-27 11:58:57 +0300

    store structure

--- src/client/client.js
+++ src/client/client.js
@@ -4,6 +4,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import AppContainer from './components/app';
+import {Dispatcher} from "shared/dispatcher";
+import * as A from "./actions";
+import createStores from "./stores";
+
+// ----------------------------
+// Services
+const dispatcher = new Dispatcher();
+const services = { dispatcher };
+
+// ----------------------------
+// Stores
+const stores = createStores(services);
+
 
 // ----------------------------
 // Render
